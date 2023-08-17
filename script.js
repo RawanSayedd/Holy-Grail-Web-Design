7d0deb957b303300fb8147f57b6e200e146d232d
RawanSayedd <rawansayed143@gmail.com>
2023-08-17 16:05:37 +0300

    last

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function toggleNav() {
   const navBar = document.querySelector("nav");
   navBar.classList.toggle("hide-nav");
 }
+
+//STYLING THE NAV BAR ICON
+
+var icons = document.querySelectorAll("#icon");
+
+for (var i = 0; i < icons.length; i++) {
+  icons[i].addEventListener("mouseenter", function () {
+    this.style.transform = "scale(1.2)";
+  });
+
+  icons[i].addEventListener("mouseleave", function () {
+    this.style.transform = "scale(1)";
+  });
+}
+
+//STYLING THE HEADER NAME/LOGO
+var typedText = document.getElementById("style-header");
+var text = "RAWAN";
+var i = 0;
+
+function type() {
+  if (i < text.length) {
+    typedText.innerHTML += text[i];
+    i++;
+    setTimeout(type, 150);
+  }
+}
+type();
